7671c32d38662595512a4b6b7183bb7934b25e0e
Cubicake <70577278+Cubicake@users.noreply.github.com>
2026-08-10 20:49:16 +0930

    Initial commit

---
+++ src/main/java/games/cubi/elytraworldborder/AntiElytraBorderDamage.java
@@ -0,0 +1,35 @@
+package games.cubi.elytraworldborder;
+
+import org.bukkit.WorldBorder;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+
+public final class AntiElytraBorderDamage extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onHitWall(EntityDamageEvent event) {
+        if (event.getCause() != EntityDamageEvent.DamageCause.FLY_INTO_WALL) return;
+        if (event.getEntity() instanceof Player player) {
+            WorldBorder worldBorder = player.getWorld().getWorldBorder();
+            Vector playerUnitVector = player.getLocation().subtract(worldBorder.getCenter()).toVector().normalize().multiply(0.1);
+            boolean atWall = !player.getWorld().getWorldBorder().isInside(player.getLocation().add(playerUnitVector));
+            if (atWall) {
+                event.setCancelled(true);
+            }
+        }
+    }
+}
